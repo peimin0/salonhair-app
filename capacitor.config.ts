107053e6dc15d9b8ac77b7ba982f1a10a16cd723
peimin0 <157945123+peimin0@users.noreply.github.com>
2026-08-31 18:33:14 +0800

    re

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.yourname.salontransparency',
+  appId: 'com.salonhsu.salontransparency',
   appName: '髮廊業績通',
   webDir: 'dist',
 };
